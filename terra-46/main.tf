d631647511eda183c27ec4873521a412c941b24a
Anil Bamnote <anilbamnote21@gmail.com>
2025-12-22 12:05:34 +0530

    tf-backend-notes-added

--- terra-46/main.tf
+++ terra-46/main.tf
@@ -8,3 +8,14 @@ module "vpc-module-basic"{
     source = "./modules/vpc-tf/"
 }
 
+
+
+terraform {
+  backend "s3" {
+    bucket         = "docker-buck-000999"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "my-first-table"
+  }
+}
